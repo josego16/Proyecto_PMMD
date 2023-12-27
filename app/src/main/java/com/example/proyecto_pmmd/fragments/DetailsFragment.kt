0a44802236289c61653a053f15c8df43621d3d60
josego16 <jgomlin@g.educaand,es>
2023-12-27 21:13:42 +0100

    terminado el toolbar menu, queda la navegacion entre los fragmentos de detalles.

--- app/src/main/java/com/example/proyecto_pmmd/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/proyecto_pmmd/fragments/DetailsFragment.kt
@@ -8,31 +8,33 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.proyecto_pmmd.R
-import com.example.proyecto_pmmd.databinding.FragmentHomeBinding
+import com.example.proyecto_pmmd.databinding.FragmentDetailsBinding
 
-class HomeFragment : Fragment() {
+class DetailsFragment : Fragment() {
     private lateinit var navController: NavController
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentDetailsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val navHost = requireActivity()
             .supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment)
         navHost.let {
             navController = navHost!!.findNavController()
 
-            binding.idBtnHomeNext.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_detailsFragment)
-            }
+            binding.idBtnData1.setOnClickListener {}
+            binding.idBtnData2.setOnClickListener {}
+            binding.idBtnData3.setOnClickListener {}
+            binding.idBtnData4.setOnClickListener {}
         }
     }
 }
--- app/src/main/java/com/example/proyecto_pmmd/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/proyecto_pmmd/fragments/RecyclerFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.proyecto_pmmd.R
 import com.example.proyecto_pmmd.controller.Controller
 import com.example.proyecto_pmmd.databinding.FragmentRecyclerBinding
 
 class RecyclerFragment : Fragment() {
+    private lateinit var navController: NavController
     private lateinit var binding: FragmentRecyclerBinding
     private lateinit var controller: Controller
 
@@ -20,6 +24,20 @@ class RecyclerFragment : Fragment() {
         binding = FragmentRecyclerBinding.inflate(layoutInflater, container, false)
         initEvent()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navhost = requireActivity()
+            .supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)
+        navhost.let {
+            navController = navhost!!.findNavController()
+
+            binding.idBtnRecyclerNext.setOnClickListener {
+                navController.navigate(R.id.action_recyclerFragment_to_detailsFragment)
+            }
+        }
     }
 
     private fun initEvent() {
